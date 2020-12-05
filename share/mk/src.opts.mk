9dfa330f145f05c2956345e26040ff7888616179
Shawn Webb <shawn.webb@hardenedbsd.org>
2020-12-05 13:42:32 -0500

    HBSD: Resolve merge conflict
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -209,11 +209,6 @@ __DEFAULT_NO_OPTIONS = \
     CLANG_FORMAT \
     DTRACE_TESTS \
     EXPERIMENTAL \
-<<<<<<< HEAD
-    FREEBSD_UPDATE \
-    GNU_GREP_COMPAT \
-=======
->>>>>>> origin/freebsd/current/master
     HESIOD \
     LIB32 \
     LIBSOFT \
