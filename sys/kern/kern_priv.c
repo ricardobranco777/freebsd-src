ricardobranco777/freebsd-src

commit fc99fc6c473cdcf23bb04d5020985c5dead3d697

HBSD: Ungate the privilege checks for KLD-related syscalls
There's no need to gate the privilege checks with HARDEN_KLD. They're
useful outside of HARDEN_KLD.

Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
issue:		#22
MFC-to:		12-STABLE
MFC-with:	155335ed8e909903445a31422c0edacd32f9f733

sys/kern/kern_priv.c
@@ -271,26 +271,19 @@ priv_check_cred(struct ucred *cred, int priv)
 		}
 	}
 
-#if !defined(HARDEN_KLD)
 	/*
-	 * Inspecting kernel module information should be root-only
-	 * when PAX_HARDENING is set.
+	 * Inspecting kernel module information should be root-only.
 	 */
 	if (priv == PRIV_KLD_STAT) {
 		error = 0;
 		goto out;
 	}
-#endif
 
 	if (priv == PRIV_SYSCTL_ROOTONLY) {
-#ifdef PAX_HARDENING
 		if (cred->cr_uid == 0) {
-#endif
 			error = 0;
 			goto out;
-#ifdef PAX_HARDENING
 		}
-#endif
 	}
 
 	/*

sys/kern/kern_module.c
@@ -314,13 +314,10 @@ sys_modnext(struct thread *td, struct modnext_args *uap)
 	module_t mod;
 	int error;
 
-#ifdef HARDEN_KLD
 	error = priv_check(td, PRIV_KLD_STAT);
 	if (error)
 		return (error);
-#else
 	error = 0;
-#endif
 
 	td->td_retval[0] = -1;
 
@@ -353,11 +350,9 @@ sys_modfnext(struct thread *td, struct modfnext_args *uap)
 	module_t mod;
 	int error;
 
-#ifdef HARDEN_KLD
 	error = priv_check(td, PRIV_KLD_STAT);
 	if (error)
 		return (error);
-#endif
 
 	td->td_retval[0] = -1;
 
@@ -393,11 +388,9 @@ sys_modstat(struct thread *td, struct modstat_args *uap)
 	struct module_stat *stat;
 	char *name;
 
-#ifdef HARDEN_KLD
 	error = priv_check(td, PRIV_KLD_STAT);
 	if (error)
 		return (error);
-#endif
 
 	MOD_SLOCK;
 	mod = module_lookupbyid(uap->modid);
@@ -449,11 +442,9 @@ sys_modfind(struct thread *td, struct modfind_args *uap)
 	char name[MAXMODNAME];
 	module_t mod;
 
-#ifdef HARDEN_KLD
 	error = priv_check(td, PRIV_KLD_STAT);
 	if (error)
 		return (error);
-#endif
 
 	if ((error = copyinstr(uap->name, name, sizeof name, 0)) != 0)
 		return (error);
@@ -502,11 +493,9 @@ freebsd32_modstat(struct thread *td, struct freebsd32_modstat_args *uap)
 	struct module_stat32 *stat32;
 	char *name;
 
-#ifdef HARDEN_KLD
 	error = priv_check(td, PRIV_KLD_STAT);
 	if (error)
 		return (error);
-#endif
 
 	MOD_SLOCK;
 	mod = module_lookupbyid(uap->modid);

commit 6b0d3fc96e23eab158d8d933f067f34f97ee2e68

HBSD: Guard KLD privilege with HARDEN_KLD
Only provide non-root access to the KLD interface with HARDEN_KLD is not
defined. This fixes a regression with commit
fc99fc6c473cdcf23bb04d5020985c5dead3d697.

Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
MFC-to:		13-STABLE
MFC-to:		12-STABLE

sys/kern/kern_priv.c
@@ -246,6 +246,7 @@ priv_check_cred(struct ucred *cred, int priv)
 		}
 	}
 
+#ifndef HARDEN_KLD
 	/*
 	 * Inspecting kernel module information should be root-only.
 	 */
@@ -253,6 +254,7 @@ priv_check_cred(struct ucred *cred, int priv)
 		error = 0;
 		goto out;
 	}
+#endif
 
 	if (priv == PRIV_SYSCTL_ROOTONLY) {
 		if (cred->cr_uid == 0) {