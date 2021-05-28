d2e652353ae9feca7a97eeb25823f6ba316686ea
Shawn Webb <shawn.webb@hardenedbsd.org>
2021-05-28 12:55:21 -0400

    HBSD: LTO-ified libraries support is not ready, yet
    I pulled the trigger a wee bit too early. The first compilation works
    fine. Attempting to do a clean build after installing the LTO-ified
    libraries fails with clang-tblgen crashing on signal 10 (bus error).
    
    Once clang is fixed, MK_LTOLIB will be defaulted to enabled again.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    Issue:		#54

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -335,11 +335,11 @@ __DEFAULT_NO_OPTIONS+=SAFESTACK
 __DEFAULT_NO_OPTIONS+=RETPOLINE
 .endif
 
-.if ${__T} == "amd64"
-__DEFAULT_YES_OPTIONS+=LTOLIB
-.else
+#.if ${__T} == "amd64"
+#__DEFAULT_YES_OPTIONS+=LTOLIB
+#.else
 __DEFAULT_NO_OPTIONS+=LTOLIB
-.endif
+#.endif
 
 .if ${__T} == "amd64" || ${__T} == "aarch64"
 __DEFAULT_YES_OPTIONS+=CFI
