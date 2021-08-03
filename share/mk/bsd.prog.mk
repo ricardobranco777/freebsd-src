edb18abddcb7a83f822cb69a4e78d98d3315bafd
Shawn Webb <shawn.webb@hardenedbsd.org>
2021-08-03 08:47:23 -0400

    HBSD: Resolve merge conflicts
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -37,56 +37,8 @@ PROG=	${PROG_CXX}
 MK_DEBUG_FILES=	no
 .endif
 
-<<<<<<< HEAD
-.include "bsd.sanitizer.mk"
-=======
-# ELF hardening knobs
-.if ${MK_BIND_NOW} != "no"
-LDFLAGS+= -Wl,-znow
-.endif
-.if ${MK_PIE} != "no"
-# Static PIE is not yet supported/tested.
-.if !defined(NO_SHARED) || ${NO_SHARED:tl} == "no"
-CFLAGS+= -fPIE
-CXXFLAGS+= -fPIE
-LDFLAGS+= -pie
-.endif
-.endif
-.if ${MK_RETPOLINE} != "no"
-.if ${COMPILER_FEATURES:Mretpoline} && ${LINKER_FEATURES:Mretpoline}
-CFLAGS+= -mretpoline
-CXXFLAGS+= -mretpoline
-# retpolineplt is broken with static linking (PR 233336)
-.if !defined(NO_SHARED) || ${NO_SHARED:tl} == "no"
-LDFLAGS+= -Wl,-zretpolineplt
-.endif
-.else
-.warning Retpoline requested but not supported by compiler or linker
-.endif
-.endif
-
-# Initialize stack variables on function entry
-.if ${MK_INIT_ALL_ZERO} == "yes"
-.if ${COMPILER_FEATURES:Minit-all}
-CFLAGS+= -ftrivial-auto-var-init=zero \
-    -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang
-CXXFLAGS+= -ftrivial-auto-var-init=zero \
-    -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang
-.else
-.warning InitAll (zeros) requested but not support by compiler
-.endif
-.elif ${MK_INIT_ALL_PATTERN} == "yes"
-.if ${COMPILER_FEATURES:Minit-all}
-CFLAGS+= -ftrivial-auto-var-init=pattern
-CXXFLAGS+= -ftrivial-auto-var-init=pattern
-.else
-.warning InitAll (pattern) requested but not support by compiler
-.endif
-.endif
-
 # bsd.sanitizer.mk is not installed, so don't require it (e.g. for ports).
 .sinclude "bsd.sanitizer.mk"
->>>>>>> origin/freebsd/current/main
 
 .if ${MACHINE_CPUARCH} == "riscv" && ${LINKER_FEATURES:Mriscv-relaxations} == ""
 CFLAGS += -mno-relax
