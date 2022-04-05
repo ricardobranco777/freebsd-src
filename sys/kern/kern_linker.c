2f7d42938e4a1f9d4dd6eb9913cd0f2286775c97
Shawn Webb <shawn.webb@hardenedbsd.org>
2022-04-05 10:44:00 -0400

    HBSD: Further harden KLD stat
    Don't report the size of kernel modules when HARDEN_KLD is set.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    issue:		#79

--- sys/kern/kern_linker.c
+++ sys/kern/kern_linker.c
@@ -1388,10 +1388,11 @@ kern_kldstat(struct thread *td, int fileid, struct kld_file_stat *stat)
 	stat->id = lf->id;
 #ifdef HARDEN_KLD
 	stat->address = NULL;
+	stat->size = 0;
 #else
 	stat->address = lf->address;
-#endif
 	stat->size = lf->size;
+#endif
 	/* Version 2 fields: */
 	namelen = strlen(lf->pathname) + 1;
 	if (namelen > sizeof(stat->pathname))
@@ -1490,10 +1491,11 @@ sys_kldsym(struct thread *td, struct kldsym_args *uap)
 		    LINKER_SYMBOL_VALUES(lf, sym, &symval) == 0) {
 #ifdef HARDEN_KLD
 			lookup.symvalue = (uintptr_t) NULL;
+			lookup.symsize = 0;
 #else
 			lookup.symvalue = (uintptr_t) symval.value;
-#endif
 			lookup.symsize = symval.size;
+#endif
 			error = copyout(&lookup, uap->data, sizeof(lookup));
 		} else
 			error = ENOENT;
@@ -1503,10 +1505,11 @@ sys_kldsym(struct thread *td, struct kldsym_args *uap)
 			    LINKER_SYMBOL_VALUES(lf, sym, &symval) == 0) {
 #ifdef HARDEN_KLD
 				lookup.symvalue = (uintptr_t)NULL;
+				lookup.symsize = 0;
 #else
 				lookup.symvalue = (uintptr_t)symval.value;
-#endif
 				lookup.symsize = symval.size;
+#endif
 				error = copyout(&lookup, uap->data,
 				    sizeof(lookup));
 				break;
