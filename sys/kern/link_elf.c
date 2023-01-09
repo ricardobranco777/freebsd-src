ricardobranco777/freebsd-src

commit 365405ba0d5fbc06f6d64ab942cdbba0c907a0d2

HBSD: fix ifdef for pax_insecure_kmod
With "nooptions HARDEN_KLD" the error
"undefined symbol: pax_insecure_kmod" was present.

Signed-off-by: Loic <loic.f@hardenedbsd.org>
MFC-to: 13-stable

sys/kern/link_elf.c
@@ -249,6 +249,7 @@ link_elf_detect_insecure_late(struct thread *td, const char *filename,
 		return (0);
 	}
 
+#ifdef HARDEN_KLD
 	if ((flags & PAX_NOTE_FORBIDKMOD) == PAX_NOTE_FORBIDKMOD ||
 	    (link_elf_kmod_is_insecure(lf) && !pax_insecure_kmod())) {
 		pax_log_internal(td->td_proc, PAX_LOG_P_COMM,
@@ -256,6 +257,7 @@ link_elf_detect_insecure_late(struct thread *td, const char *filename,
 		    filename != NULL ? filename : "<unknown>");
 		return (EPERM);
 	}
+#endif
 
 	return (0);
 }

sys/kern/link_elf_obj.c
@@ -229,6 +229,7 @@ link_elf_detect_insecure_late(struct thread *td, const char *filename,
 		return (0);
 	}
 
+#ifdef HARDEN_KLD
 	if ((flags & PAX_NOTE_FORBIDKMOD) == PAX_NOTE_FORBIDKMOD ||
 	    (link_elf_kmod_is_insecure(lf) && !pax_insecure_kmod())) {
 		pax_log_internal(td->td_proc, PAX_LOG_P_COMM,
@@ -236,6 +237,7 @@ link_elf_detect_insecure_late(struct thread *td, const char *filename,
 		    filename != NULL ? filename : "<unknown>");
 		return (EPERM);
 	}
+#endif
 
 	return (0);
 }

commit aa75ca15f703473e31b7c35438f96989a298480a

HBSD: Fix insecure kmod loading enforcement
Commit 365405ba0d5fbc06f6d64ab942cdbba0c907a0d2 introduced a regression
that broke insecure kmod loading enforcement.

Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
MFC-to:		13-STABLE

sys/kern/link_elf.c
@@ -31,6 +31,7 @@ __FBSDID("$FreeBSD$");
 
 #include "opt_ddb.h"
 #include "opt_gdb.h"
+#include "opt_pax.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>

sys/kern/link_elf_obj.c
@@ -31,6 +31,7 @@
 __FBSDID("$FreeBSD$");
 
 #include "opt_ddb.h"
+#include "opt_pax.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>