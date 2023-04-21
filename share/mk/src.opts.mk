410f0a434c4e9f569753154a401b11546ad0ca82
Shawn Webb <shawn.webb@hardenedbsd.org>
2023-04-21 13:36:13 -0400

    HBSD: Resolve merge conflicts
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -209,7 +209,6 @@ __DEFAULT_NO_OPTIONS = \
     LOADER_VERBOSE \
     LOADER_VERIEXEC_PASS_MANIFEST \
     MALLOC_PRODUCTION \
-    NETLINK \
     NETLINK_SUPPORT \
     OFED \
     OFED_EXTRA \
@@ -393,15 +392,6 @@ __DEFAULT_NO_OPTIONS+=OPENMP
 BROKEN_OPTIONS+= OFED
 .endif
 
-<<<<<<< HEAD
-=======
-.if ${__T} == "i386" || ${__T} == "amd64"
-__DEFAULT_YES_OPTIONS+=NETLINK_SUPPORT
-.else
-__DEFAULT_NO_OPTIONS+=NETLINK_SUPPORT
-.endif
-
->>>>>>> freebsd/main
 .-include <site.src.opts.mk>
 
 .include <bsd.mkopt.mk>
