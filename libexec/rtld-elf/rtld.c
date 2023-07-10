51fda68c1c076e4e48e76b443366068fb8de3d1e
Shawn Webb <shawn.webb@hardenedbsd.org>
2023-07-10 08:40:07 -0600

    HBSD: Resolve merge conflict
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -869,15 +869,10 @@ _rtld(Elf_Addr *sp, func_ptr_type *exit_proc, Obj_Entry **objp)
     dbg("obj_main path %s", obj_main->path);
     obj_main->mainprog = true;
 
-<<<<<<< HEAD
-#ifndef COMPAT_32BIT
-=======
+#ifndef COMPAT_libcompat
     if (aux_info[AT_STACKPROT] != NULL &&
       aux_info[AT_STACKPROT]->a_un.a_val != 0)
 	    stack_prot = aux_info[AT_STACKPROT]->a_un.a_val;
-
-#ifndef COMPAT_libcompat
->>>>>>> freebsd/main
     /*
      * Get the actual dynamic linker pathname from the executable if
      * possible.  (It should always be possible.)  That ensures that
