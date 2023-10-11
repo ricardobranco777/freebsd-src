5e443a8b8d6b039452246da6d9665062ed0976fc
Shawn Webb <shawn.webb@hardenedbsd.org>
2023-10-11 14:36:22 -0600

    HBSD: Resolve merge conflicts
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/arm/arm/elf_machdep.c
+++ sys/arm/arm/elf_machdep.c
@@ -90,13 +90,8 @@ struct sysentvec elf32_freebsd_sysvec = {
 	.sv_fixlimit	= NULL,
 	.sv_maxssiz	= NULL,
 	.sv_flags	=
-<<<<<<< HEAD
 			  SV_SHP | SV_TIMEKEEP | SV_RNG_SEED_VER |
-			  SV_ABI_FREEBSD | SV_ILP32,
-=======
-			  SV_ASLR | SV_SHP | SV_TIMEKEEP | SV_RNG_SEED_VER |
 			  SV_ABI_FREEBSD | SV_ILP32 | SV_SIGSYS,
->>>>>>> origin/freebsd/14-stable/main
 	.sv_set_syscall_retval = cpu_set_syscall_retval,
 	.sv_fetch_syscall_args = cpu_fetch_syscall_args,
 	.sv_syscallnames = syscallnames,
